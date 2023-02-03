9f147517a3cdd7b92125b968126bde018c39d1f2
George Britton <george.britton97@hotmail.com>
2023-02-03 09:30:27 +0000

    Initial commit

---
+++ Source/RootsGGJ23/RootsGGJ23GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RootsGGJ23GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOTSGGJ23_API ARootsGGJ23GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
